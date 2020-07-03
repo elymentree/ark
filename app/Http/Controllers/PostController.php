99dfcb5de0ea76130769adf6f855d0ae90c539f3
elizarflores <elizar@factorytiledepot.ca>
2020-07-03 15:37:43 -0400

    Working post test with Json data structure.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
    public function store()
    {
-		return response([],201);
+   	    $data = request()->validate([
+   	    	'data.attributes.body' =>'', // SHORTCUT FOR NESTED JSON STRUCTURE
+        ]);
+
+   	   // dd($data);
+   	    // user() [MODEL] -> posts() [METHOD]
+   	    // $post  = request()->user()->posts()->create($data);
+   	    $post  = request()->user()->posts()->create($data['data']['attributes']);  // TO POINT TO ATTRIBUTES
+
+		return response([
+			'data' => [
+				'type' => 'post',
+				'post_id' => $post->id,
+				'attributes'=>[
+					'body' =>$post->body,
+				]
+			],
+			'links' =>[
+				'self' => url('/posts/' .$post->id)
+			]
+
+		],201);
    }
 
 }
